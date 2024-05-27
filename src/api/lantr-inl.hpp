2cfde2c071fa569a163337587d7146588d8826d1
NzSN <nzsn0101@gmail.com>
2024-05-27 17:39:34 +0800

    fixup. Give a short alias name for longnames.

--- src/api/lantr-inl.hpp
+++ src/api/lantr-inl.hpp
@@ -4,13 +4,16 @@
 #include "lantr.hpp"
 #include "base/utility.hpp"
 
-namespace Bottom = LANTr::Base::Utility::Bottom;
-
 namespace LANTr::Api {
+
+namespace {
+using Base::Utility::Bottom::Unreachable;
+using Base::Utility::Bottom::Reason;
+}
 
 template<LANG lang>
 Source LANTr<lang>::trans(Source s) {
-  Bottom::Unreachable(Bottom::Reason::NOT_IMPLEMENTED);
+  Unreachable(Reason::NOT_IMPLEMENTED);
 }
 
 } // LANTr::Api
