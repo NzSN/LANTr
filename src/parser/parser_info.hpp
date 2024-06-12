c34e07794dcac30f09337a5c31091906b36ea8e0
NzSN <nzsn0101@gmail.com>
2024-06-12 19:58:31 +0800

    Add Transformer module

---
+++ src/parser/parser_info.hpp
@@ -0,0 +1,14 @@
+#ifndef LANGR_PARSER_PARSER_INFO_H_
+#define LANGR_PARSER_PARSER_INFO_H_
+
+#include "parser/languages/language_definitions.hpp"
+
+namespace LANTr::Parser {
+
+template<LANGUAGE::LANGUAGE L>
+struct ParserInfo;
+
+} // LANTr::PARSER
+
+
+#endif /* LANGR_PARSER_PARSER_INFO_H_ */
