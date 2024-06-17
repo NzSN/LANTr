3300a74a91faf4bcf8a5f1d399ac5bb7a8bef60c
NzSN <nzsn0101@gmail.com>
2024-06-17 12:10:20 +0800

    fixup

--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -1,11 +1,34 @@
 #ifndef LANTR_PARSER_PARSER_H_
 #define LANTR_PARSER_PARSER_H_
 
+#include "base/types.hpp"
+#include "parser/languages/language_definitions.hpp"
+#include "parser/languages/language_impl.hpp"
+
+#include "parser/parser_info.hpp"
+
 namespace LANTr::Parser {
 
-class Parser {};
+template<LANGUAGE::LANGUAGE L>
+class [[nodiscard]] ParseResult {
+public:
+
+
+private:
+  using ParseResult_Unique =
+    std::unique_ptr<LANGUAGE::ParseResult<L>>;
+
+  ParseResult_Unique result;
+
+
+};
+
+template<LANGUAGE::LANGUAGE L>
+class Parser {
+public:
+  ParseResult<L> Parse(Base::Types::Source source);
+};
 
 } // LANTr::Parser
-
 
 #endif // LANTR_PARSER_PARSER_H_
