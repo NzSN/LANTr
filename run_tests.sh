10f555cd4bb4431f73279ca440a978ae66bdeb37
NzSN <nzsn0101@gmail.com>
2024-06-24 11:47:12 +0800

    Update run_test.sh to detect OS type.

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-bazel test $(bazel query //...) -c dbg --config=$1 --build_tests_only
+
+CONFIG=debug
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    CONFIG=debug_on_mac
+fi
+
+bazel test $(bazel query //...) -c dbg --config=$CONFIG --build_tests_only
